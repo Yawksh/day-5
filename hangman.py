056c539d49291a94bf88405c47a6baadb753e557
Yawksh <152399244+Yawksh@users.noreply.github.com>
2024-01-31 20:40:23 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 
 import random, time
+
 fruits = ['per', 'mango', 'apple', 'banana', 'apricot', 'pineapple','cantaloupe', 'grapefruit','jackfruit','papaya']
 superHeroes = ['hawkeye', 'robin', 'Galactus', 'thor', 'mystique', 'superman', 'deadpool', 'vision', 'sandman', 'aquaman']
 userGuesslist = []
